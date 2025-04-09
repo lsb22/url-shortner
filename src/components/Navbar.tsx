a4a5fa1ba5c6e7042d249b2085a372d1e32fddd0
lsb <likith15600@gmail.com>
2025-04-09 17:02:54 +0530

    Added logout functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,26 @@
+import { useNavigate } from "react-router";
+
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleLogOutClick = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+  };
   return (
-    <div className="flex justify-center p-2 items-center text-2xl gap-5 border-b-2">
-      <h2>HomePage</h2>
-      <p>|</p>
-      <h2>Statistics</h2>
+    <div className="flex justify-between p-2 items-center text-2xl  border-b-2">
+      <div className="">user1</div>
+      <div className="flex gap-5">
+        <h2>HomePage</h2>
+        <p>|</p>
+        <h2>Statistics</h2>
+      </div>
+      <button
+        className="bg-white text-lg text-black py-1 px-2 border-3 rounded-md"
+        onClick={handleLogOutClick}
+      >
+        Logout
+      </button>
     </div>
   );
 };
