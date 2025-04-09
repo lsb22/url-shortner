c07d563e8cc7622555e065cf1b8246ad13205b46
lsb <likith15600@gmail.com>
2025-04-09 16:52:25 +0530

    Added navbar

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,5 +1,12 @@
+import Navbar from "./Navbar";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Navbar />
+      HomePage
+    </div>
+  );
 };
 
 export default HomePage;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div className="flex justify-center p-2 items-center text-2xl gap-5 border-b-2">
+      <h2>HomePage</h2>
+      <p>|</p>
+      <h2>Statistics</h2>
+    </div>
+  );
+};
+
+export default Navbar;
